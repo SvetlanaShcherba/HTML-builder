106e2cb51fca088a5981016446d295d6a919eaab
shcherba sviatlana <shcherba.svetlana4@gmail.com>
2023-05-03 16:46:17 +0300

    feat: add a solution to the task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const textPath = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(textPath);
+stdout.write('Введите текст\n');
+stdin.on('data', data => {
+  if (data.toString().trim() !== 'exit') {
+    output.write(data);
+    stdout.write('Введите текст\n');    
+  } else {
+    stdout.write('Всего хорошего!');
+    process.exit();
+  }
+});
