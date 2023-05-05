157656618cfb3792c86180bd25892b5010debad3
shcherba sviatlana <shcherba.svetlana4@gmail.com>
2023-05-05 20:53:53 +0300

    feat: add a solution to the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir, copyFile, mkdir } =  require('fs').promises;
+
+const dirrPath = path.join(__dirname, 'files');
+const newDirrPath = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  const filesDirr = await readdir(dirrPath, {withFileTypes: true});
+    const newFolder = await mkdir(newDirrPath, {recursive: true,});
+    for (const file of filesDirr) {
+      try {
+        const filePath = path.join(__dirname, 'files', file.name);
+        const newFilePath = path.join(__dirname, 'files-copy', file.name);
+        await copyFile(filePath, newFilePath);
+      } catch {
+        console.error('Файлы не удалось скопировать');
+      }
+    }
+  }
+copyDir().catch(console.error);
 
