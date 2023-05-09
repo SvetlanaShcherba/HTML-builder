b87744c8116f9b03b42b760c6581a73da39e4735
shcherba sviatlana <shcherba.svetlana4@gmail.com>
2023-05-09 22:41:04 +0300

    fix: fix a solution to the task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,9 +8,13 @@ stdout.write('Введите текст\n');
 stdin.on('data', data => {
   if (data.toString().trim() !== 'exit') {
     output.write(data);
-    stdout.write('Введите текст\n');    
+    stdout.write('Введите текст\n');
   } else {
     stdout.write('Всего хорошего!');
     process.exit();
   }
+});
+process.on('SIGINT', () => {
+  stdout.write('Всего хорошего!');
+  process.exit();
 });
