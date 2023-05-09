a476a776df5d7b26f2e3dab83b789490bfd98b2e
shcherba sviatlana <shcherba.svetlana4@gmail.com>
2023-05-09 22:10:45 +0300

    fix: fix a solution to the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,27 @@
 const fs = require('fs');
 const path = require('path');
-const { readdir, copyFile, mkdir } =  require('fs').promises;
+const { readdir, copyFile, mkdir, rm} =  require('fs').promises;
 
 const dirrPath = path.join(__dirname, 'files');
 const newDirrPath = path.join(__dirname, 'files-copy');
 
 async function copyDir() {
   const filesDirr = await readdir(dirrPath, {withFileTypes: true});
-    const newFolder = await mkdir(newDirrPath, {recursive: true,});
-    for (const file of filesDirr) {
-      try {
-        const filePath = path.join(__dirname, 'files', file.name);
-        const newFilePath = path.join(__dirname, 'files-copy', file.name);
-        await copyFile(filePath, newFilePath);
-      } catch {
-        console.error('Файлы не удалось скопировать');
-      }
+  const newFolder = await mkdir(newDirrPath, {recursive: true,});
+  const filesNewDir = await readdir(newDirrPath, {withFileTypes: true});
+  for (const file of filesNewDir){
+    const filePath = path.join(newDirrPath, file.name);
+    await rm(filePath);
+  }
+  for (const file of filesDirr) {
+    try {
+      const filePath = path.join(dirrPath, file.name);
+      const newFilePath = path.join(newDirrPath, file.name);
+      await copyFile(filePath, newFilePath);
+    } catch {
+      console.error('Файлы не удалось скопировать');
     }
   }
+}
 copyDir().catch(console.error);
 
